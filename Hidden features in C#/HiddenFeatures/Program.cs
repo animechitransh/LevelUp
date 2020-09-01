ff85948a7eea241ae67592786f33b059a1c45ac5
Chitransh <animechitransh@gmail.com>
2020-09-02 03:12:39 +0530

    Adding comments

--- Hidden features in C#/HiddenFeatures/Program.cs
+++ Hidden features in C#/HiddenFeatures/Program.cs
@@ -6,6 +6,11 @@ using System.Threading.Tasks;
 
 namespace HiddenFeatures
 {
+    /// <summary>
+    /// Teacher: https://github.com/badamczewski?tab=repositories&q=&type=&language=
+    /// Youtube: https://www.youtube.com/channel/UCw6BnEkURLprFGx_il2OyUw
+    /// Video: https://www.youtube.com/watch?v=B_69UvFFyvY
+    /// </summary>
     class Program
     {
         static void Main(string[] args)
